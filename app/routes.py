e3dfab53ab57a88f4aa1d5fdd4d9bb42d908106d
Pincenti <ctegs@comcast.net>
2021-05-28 00:11:59 -0400

    first commit

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,14 @@
 from app import app
-from flask import render_template, redirect
-
-from .forms import SampleForm
+from flask import redirect, jsonify
 
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
-    return render_template('index.html')
+    return jsonify({})
 
 
-@app.route('/articles')
-def articles():
-    return render_template('articles.html')
-
-
-@app.route('/forms', methods=['GET', 'POST'])
-def forms():
-    form = SampleForm()
-
-    # If form is upon submission and is validated success
-    if form.validate_on_submit():
-        return redirect('index')
-
-    return render_template('forms.html', form=form)
+@app.route('/products')
+def products():
+    return jsonify({})
